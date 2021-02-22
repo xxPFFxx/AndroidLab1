9dc54120f60114ec132808e84b2eca6f74ce27c3
Daniil Demichev <daniil_demichev@mail.ru>
2021-02-22 08:47:30 +0300

    ActionBars

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.lab1
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mActionBarToolbar: Toolbar = findViewById<View>(R.id.toolbar_actionbarmain) as Toolbar
+        setSupportActionBar(mActionBarToolbar)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
     }
 }
