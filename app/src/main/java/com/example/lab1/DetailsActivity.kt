dcf8dd3f7514adfd0cbeda9f939c367c3d5d7559
Daniil Demichev <daniil_demichev@mail.ru>
2021-04-03 12:38:46 +0300

    Lab4

---
+++ app/src/main/java/com/example/lab1/DetailsActivity.kt
@@ -0,0 +1,16 @@
+package com.example.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.menu, menu)
+//        return true
+//    }
+}
